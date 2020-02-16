ad02b2816bba2248dbd617fcd4301b1185362701
Can Koz <canxkoz@gmail.com>
2020-02-15 18:27:20 -0800

    Starting
    blank program

---
+++ iMove/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iMove
+//
+//  Created by Can Koz on 2/15/20.
+//  Copyright © 2020 Can Koz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iMoveTests/iMoveTests.swift
@@ -0,0 +1,34 @@
+//
+//  iMoveTests.swift
+//  iMoveTests
+//
+//  Created by Can Koz on 2/15/20.
+//  Copyright © 2020 Can Koz. All rights reserved.
+//
+
+import XCTest
+@testable import iMove
+
+class iMoveTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
